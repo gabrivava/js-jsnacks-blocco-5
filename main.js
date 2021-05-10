063e9f7a9ba8f986733daea1f4b083d95165854f
gabrivava <gabriele.vavassori4@gmail.com>
2021-05-10 15:31:12 +0200

    complete jsnack 2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Snack 1:
+//**************Snack 1:
 //Crea un array di 10 oggetti che rappresentano una zucchina, indicando per ognuna varietà, peso e lunghezza.
 
 var zucchine = [
@@ -22,7 +22,31 @@ for (var i = 0; i < zucchine.length; i++) {
 }
 console.log(pesoZucchine);
 
-//Snack 2:
-//Crea 10 oggetti che rappresentano una zucchina.
+//creato funzione da poter riutilizzare
+function sommaPeso(array) {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        var n = Number(array[i].peso);
+        sum += n;
+    }
+    return sum
+}
+
+
+//************Snack 2:
 //Dividi in due array separati le zucchine che misurano meno o più di 15cm.
+var zucchineGrandi = [];
+var zucchinePiccole = [];
+for (var i = 0; i < zucchine.length; i++) {
+    var singola = zucchine[i].lunghezza;
+    if (singola <= 15) {
+        zucchinePiccole.push(zucchine[i])
+    } else {
+        zucchineGrandi.push(zucchine[i])
+    }
+}
+console.log(zucchineGrandi);
+console.log(zucchinePiccole);
 //Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+console.log(sommaPeso(zucchineGrandi));
+console.log(sommaPeso(zucchinePiccole));
